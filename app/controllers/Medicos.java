9cb65d4cd9b12f4b7751ef68ac14fbe113c46e15
VITOR GABRIEL NASCIMENTO TEIXEIRA <20221064010015@ifrn.local>
2025-07-29 17:22:37 -0300

    errooo

--- app/controllers/Medicos.java
+++ app/controllers/Medicos.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import models.Medico;
+import models.Status;
 import play.mvc.Controller;
 
 public class Medicos extends Controller{
@@ -10,30 +11,44 @@ public class Medicos extends Controller{
 	public static void form() {
 		render();
 	}
-	public static void listar() {
-		List<Medico> lista = Medico.findAll();
-		render(lista);
+	
+	public static void listar(String termo) {
+	    List<Medico> medicosAtivos;
+
+	    if (termo == null || termo.trim().isEmpty()) {
+	        medicosAtivos = Medico.find(
+	            "status <> ?1 or status is null",
+	            Status.INATIVO
+	        ).fetch();
+	    } else {
+	        String termoBusca = "%" + termo.toLowerCase() + "%";
+	        medicosAtivos = Medico.find(
+	            "lower(nome) like ?1 and (status <> ?2 or status is null)",
+	            termoBusca, Status.INATIVO
+	        ).fetch();
+	    }
+
+	    render(medicosAtivos, termo);
 	}
+
 	
 	public static void salvar(Medico med) {
 	
 		med.save();
-		listar();
+//      listar(null);		
+		form();
 	}
 	
 	public static void editar(Long id) {
 		
 		Medico med = Medico.findById(id); 
-		if(med == null) {
-          flash.error("Médico não está inserido.");	
-          renderTemplate("Medicos/listar.html");
-		}
 		renderTemplate("Medicos/form.html", med);
 	}
 	
 	public static void deletar(Long id) {
 		Medico med = Medico.findById(id);
-		med.delete();
-	    listar();
+		med.status = Status.INATIVO;
+		med.save();
+	    listar(null);
 	}
 }
